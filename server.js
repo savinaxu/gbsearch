ea575bc465ea605ad1065865d70a9b1e808f90ff
savinaxu <jia_shijie@163.com>
2019-03-02 12:06:59 -0800

    ready for deploy.

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 // Add routes, both API and view
@@ -18,7 +21,7 @@ app.use(routes);
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch"
 
-mongoose.Promise = Promise;
+// mongoose.Promise = Promise;
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }).catch(e => {
   console.log(e);
