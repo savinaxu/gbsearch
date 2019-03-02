433d54f6ed743d1e59af0b7f7ef5e6329c60f1a3
savinaxu <jia_shijie@163.com>
2019-03-02 12:20:56 -0800

    added path.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
+const path = require("path")
 const app = express();
 
 // Define middleware here
